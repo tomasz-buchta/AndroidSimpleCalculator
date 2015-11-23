63565efbd7d66f287be65e86eb8881cc4b26f0b3
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-11-23 12:47:56 +0100

    Added debug logging

--- app/src/main/java/com/example/tbuchta/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/tbuchta/simplecalculator/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.tbuchta.simplecalculator;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     EditText editText1;
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         result = (TextView)findViewById(R.id.textView2);
         calculateButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+                Log.d("MainActivity","Calculate " + editText1.getText().toString() + " and " + editText2.getText().toString());
                 Integer a = Integer.parseInt(editText1.getText().toString());
                 Integer b = Integer.parseInt(editText2.getText().toString());
                 Integer c = a*b;
